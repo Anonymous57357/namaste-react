9c373e839cdb2063c34eabfcb53b9616fa629058
mukeshvijayakumar <mukesh@techasoft.com>
2024-12-17 12:41:29 +0530

    episode-01

---
+++ Episode 01 -  Inception/code/app.js
@@ -0,0 +1,52 @@
+const heading = React.createElement(
+  "h1",
+  { id: "head" },
+  "this heading is from react"
+); // is a object () props
+
+// console.log(heading);
+
+/**
+ *
+ */
+
+const heading2 = React.createElement(
+  "div", // TAB
+  { id: "parent" }, // OBJECT (ATTRIBUTE)
+  React.createElement("div", { id: "child" }, [
+    // array of children
+    React.createElement("h2", {}, "am a child h1 element"),
+    React.createElement("h2", {}, "am a Sibling h1 element"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    // array of children
+    React.createElement("h2", {}, "am a child h1 element"),
+    React.createElement("h2", {}, "am a sibling h1 element"),
+  ])
+);
+
+const heading3 = React.createElement(
+  "h3",
+  { id: "parent" },
+  React.createElement("h3", { id: "child" }, [
+    React.createElement("h3", {}, "am a child eldment"),
+    React.createElement("h3", {}, "am a child eldment"),
+  ]),
+  React.createElement("h3", { id: "child" }, [
+    React.createElement("h3", {}, "am a child eldment"),
+    React.createElement("h3", {}, "am a child eldment"),
+  ])
+);
+
+// JSX
+
+console.log(heading3);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(root);
+
+root.render(heading3);
+
+
+// everything inside the id (root) elment is replace by react not upend 
---
+++ Episode 01 -  Inception/theory/Episode 01 -  Inception.md
@@ -0,0 +1,9 @@
+## HOW REACT IS FAST
+
+CDN stands for Content Delivery Network, which is a group of servers that work together to deliver content to users more quickly: 
+How it works
+When a user visits a website, the data from the website's server needs to travel across the internet to the user's computer. A CDN stores copies of the website's content on servers that are closer to the user, so the content loads faster. 
+
+React.CreateElement is a object --> its a javascript object
+
+root.render ==> is rendered a object and replace by h1 or any html tag
